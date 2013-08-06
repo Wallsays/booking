f089103b1b2b620b5ed9892a7a9f7d7803804ab0
Dis <sr_5@inbox.ru>
2013-08-07 02:37:54 +0700

    production fix

--- db/migrate/20130806170205_change_reservations_table.rb
+++ db/migrate/20130806170205_change_reservations_table.rb
@@ -1,6 +1,6 @@
 class ChangeReservationsTable < ActiveRecord::Migration
   def change
-    remove_column :reservations, :user
+    # remove_column :reservations, :user
     remove_column :reservations, :restaurant
     remove_column :reservations, :reservationable_id
     remove_column :reservations, :reservationable_type
