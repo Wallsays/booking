7ed232bc866a73cb06b075cf01e865bc6831772a
Wallsays <derevyankodenis92@gmail.com>
2013-08-08 17:02:24 +0700

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -8,13 +8,13 @@ Rails.application.config.middleware.use OmniAuth::Builder do
       # provider :google_oauth2, "APP_ID", "APP_SECRET", { access_type: "offline", approval_prompt: "" }
 
     when "production"
-      provider :facebook, '515632468516870', '12eb33be0069042efb658537afdc7f2a'
+      # provider :facebook, '515632468516870', '12eb33be0069042efb658537afdc7f2a'
       # provider :facebook, '215666451922396', '80c8b913e94e0bace9d5dcc7005f91c0'
 
       # config.omniauth :facebook, "215666451922396", "80c8b913e94e0bace9d5dcc7005f91c0",
       #     :client_options => {:ssl => {:ca_path => '/etc/ssl/certs'}}
 
-      config.omniauth :facebook, "515632468516870", "12eb33be0069042efb658537afdc7f2a",
+      provider :facebook, "515632468516870", "12eb33be0069042efb658537afdc7f2a",
           {:scope => 'email, offline_access', 
             :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}} 
   end
